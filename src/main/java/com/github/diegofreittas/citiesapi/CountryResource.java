796b23249fe170b7584f353448c51da46404923d
diego-freittas <tpddiego@gmail.com>
2021-07-10 15:54:07 -0300

    Implementacao do listByID na tabela pais

--- src/main/java/com/github/diegofreittas/citiesapi/CountryResource.java
+++ src/main/java/com/github/diegofreittas/citiesapi/CountryResource.java
@@ -3,13 +3,18 @@ package com.github.diegofreittas.citiesapi;
 import com.github.diegofreittas.citiesapi.countries.Country;
 import com.github.diegofreittas.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -19,7 +24,18 @@ public class CountryResource {
     private CountryRepository repository;
 
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    //http://localhost:8080/countries?page=0&size=10&sort=name,asc exemplo de uma requisicao GET paginada
+    //http://localhost:8080/countries?page=2&size=10&sort=id,asc
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity findByID(@PathVariable Long id){
+        Optional<Country> opiCountry = repository.findById(id);
+        if(opiCountry.isPresent()) {
+            return ResponseEntity.ok().body(opiCountry.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
